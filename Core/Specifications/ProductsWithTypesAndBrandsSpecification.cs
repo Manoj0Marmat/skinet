d7f2bec155b80f4dff66a3e6665d92a94344353a
Manoj Marmat <manojrajput762000@gmail.com>
2023-09-07 23:01:22 +0530

    Implemented mapper, dto and generic repo

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddIncludes(p => p.ProductBrand);
+            AddIncludes(p => p.ProductType);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) 
+            : base(p => p.Id == id)
+        {
+            AddIncludes(p => p.ProductBrand);
+            AddIncludes(p => p.ProductType);
+        }
+    }
+}
