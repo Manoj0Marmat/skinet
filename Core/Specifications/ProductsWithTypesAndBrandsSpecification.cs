349d194db585e617ecdd4436fbdc3037e3222f3e
Manoj Marmat <manojrajput762000@gmail.com>
2023-10-25 07:16:10 +0530

    Feat: adding filter func to specification

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,18 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 using Core.Entities;
 
 namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId):
+        base(x => (
+            (!brandId.HasValue) || x.ProductBrandId == brandId)
+            &&
+            ((!typeId.HasValue) || x.ProductTypeId == typeId ))
         {
             AddIncludes(p => p.ProductBrand);
             AddIncludes(p => p.ProductType);
+            AddOrderBy(p => p.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int id) 
